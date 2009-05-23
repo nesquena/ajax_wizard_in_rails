10c30a4881ab8ad3396cb24f0d41d1d44c12c1f2
Nathan Esquenazi <nesquena@gmail.com>
2009-05-22 23:53:23 -0700

    updated layout with new navigation links

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -1,4 +1,4 @@
 external = [ 'libraries/protoculous', 'libraries/lowpro',
-             'libraries/animator', 'extensions/animator_extensions']
+             'libraries/animator', 'libraries/animator_extensions']
               
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :external => external
