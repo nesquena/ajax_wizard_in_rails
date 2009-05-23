ef696f18ea2a310a71c014447aaaa40b123dfbf9
Nathan Esquenazi <nesquena@gmail.com>
2009-05-23 14:17:22 -0700

    refactored javascript for building computer review into separate behavior for better readability

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,12 @@
 WizardForm = Behavior.create({
 	initialize : function() {
+	  this.reviewEl = new WizardForm.Review(this.element.down('#review'));
 	  this.setDefaults();
 		this.activateSection("colors");
-		this.updateReview(); 
 	},
 	onclick : Event.delegate({
 		'a.section-link, a.section-link *' : function(e) { this.activateSection(e.findElement('a').readAttribute('rel')); },
-		'input[type=radio]' : function(e) { this.updateReview(); }
+		'input[type=radio]' : function(e) { this.reviewEl.updateReview(); }
 	}),
 	activateSection : function(sectionName) {
 		this.element.select('.section').invoke('removeClassName', 'active');
@@ -14,27 +14,38 @@ WizardForm = Behavior.create({
 		this.element.down('.' + sectionName + '.section').addClassName('active');
 		this.element.down('.' + sectionName + '-nav').addClassName('selected');
 	},
-	updateReview : function() {
-	  var totalPrice = 0;
-	  var componentsList = this.element.up('#customize-computer').down('#review ul.components');
-	  var priceEl = this.element.up('#customize-computer').down('#review span.price');
-	  var priceField = this.element.up('#customize-computer').down('#review input.price');
-	  componentsList.update('');
-	  this.element.select('input[type=radio]').each(function(inputEl) {
-	    var description = inputEl.next('label').down('.option-name').innerHTML;
-	    var price = inputEl.next('label').down('.option-price').innerHTML.match(/\d+/)[0];
-  	  if (inputEl.checked) { 
-  	    totalPrice += parseInt(price, 10); 
-  	    componentsList.insert($li(description));
-  	  }
-	  }.bind(this));
-	  priceEl.update('$' + totalPrice);
-	  priceField.value = totalPrice;
-	},
 	setDefaults : function() {
 	  this.element.select('.section').each(function(inputEl) {
 	    inputEl.down('input').checked = true;
     });
+	}
+});
+
+WizardForm.Review = Behavior.create({
+  initialize : function() {
+    this.componentsList = this.element.down('ul.components');
+	  this.updateReview(); 
+  },
+  updateReview : function() {
+    this._calculatePrice(); 
+    this._buildComponentsList();
+	},
+	_calculatePrice : function() {
+	  var totalPrice = 300;
+	  this._eachSelectedOption(function(inputEl) { totalPrice += inputEl.price; });
+	  this.element.down('span.price').update('$' + totalPrice);
+	  this.element.down('input.price').value = totalPrice;
+	},
+	_buildComponentsList : function() {
+	  this.componentsList.update('');
+	  this._eachSelectedOption(function(inputEl) { this.componentsList.insert($li(inputEl.desc)); }.bind(this));
+	},
+	_eachSelectedOption : function(callBack) {
+	  this.element.previous('#design').select('input[type=radio]:checked').each(function(inputEl) {
+	    inputEl.price = parseInt(inputEl.next('label').down('.option-price').readAttribute('rel'), 10);
+	    inputEl.desc = inputEl.next('label').down('.option-name').innerHTML;
+	    callBack(inputEl); 
+	  }.bind(this));
 	}
 });
 
