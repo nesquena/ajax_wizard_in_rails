97823616c69c1f80b426ed6789ac0695f6df416a
Nathan Esquenazi <nesquena@gmail.com>
2009-05-22 23:52:43 -0700

    added index and create actions to computer controller

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -1,6 +1,30 @@
 class ComputersController < ApplicationController
+  def index
+    @computers = Computer.all
+    respond_to(:html) # index.html.haml
+  end
+
   def new
     @computer = Computer.new
     respond_to(:html) # new.html.haml
+  end
+
+  def create
+    @computer = Computer.new(params[:computer])
+    respond_to do |wants|
+      if @computer.save
+        wants.html { redirect_to :action => 'index'  }
+      else
+        wants.html { render :action => 'new'  }
+      end
+    end
+  end
+  
+  def destroy
+    @computer = Computer.find(params[:id])
+    @computer.destroy
+    respond_to do |wants|
+      wants.html { redirect_to :action => 'index' }
+    end
   end
 end
